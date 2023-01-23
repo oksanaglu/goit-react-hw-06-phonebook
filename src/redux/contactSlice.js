2396f436fe0aadd3830af68a5e643b591dd1dd81
Oksana Gluschenko <oksana_a_g@ukr.net>
2023-01-23 22:34:05 +0200

    correct

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,22 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit'
+// import { createSlice } from '@reduxjs/toolkit'
 
-export const contactSlice = createSlice({
-  name: 'contacts',
-  initialState,
-  reducers: {
-    increment: (state, action) => {
+// export const contactSlice = createSlice({
+//   name: 'contacts',
+//   initialState,
+//   reducers: {
+//     increment: (state, action) => {
    
-      state.value += 1
-    },
-    decrement: (state) => {
-      state.value -= 1
-    },
-    incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    },
-  },
-})
+//       state.value += 1
+//     },
+//     decrement: (state) => {
+//       state.value -= 1
+//     },
+//     incrementByAmount: (state, action: PayloadAction<number>) => {
+//       state.value += action.payload
+//     },
+//   },
+// })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+// export const { increment, decrement, incrementByAmount } = counterSlice.actions
 
-export default counterSlice.reducer
+// export default counterSlice.reducer
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { createAction, createReducer } from '@reduxjs/toolkit'
+// import { createAction, createReducer } from '@reduxjs/toolkit'
 
 export const store = configureStore({
   reducer: {},
