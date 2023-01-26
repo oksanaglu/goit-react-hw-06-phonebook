45bbedc539fbc545e63b508728f85119324ae5b3
Oksana Gluschenko <oksana_a_g@ukr.net>
2023-01-26 22:10:49 +0200

    fix

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -10,12 +10,12 @@ export const Section = styled.section`
 export const Title1 = styled.h1`
   margin-bottom: 10px;
   font-size: 26px;
-  font-weight: bold;
+ ;
 `;
 
 export const Title2 = styled.h2`
   margin-bottom: 10px;
   font-size: 22px;
-  font-weight: bold;
+ 
 `;
 
